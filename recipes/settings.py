4e4c52f398576eb17c14acdc5e4a4ee3f8c3bafc
r3econ <rafal.sroka.it@gmail.com>
2017-12-01 21:50:30 +0100

    Rename apps

--- recipes/settings.py
+++ recipes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEST_RUNNER = 'gettingstarted.heroku_test_runner.HerokuDiscoverRunner'
+TEST_RUNNER = 'recipes.heroku_test_runner.HerokuDiscoverRunner'
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes_website',
+    'website',
     'rest_framework',
 )
 
@@ -57,7 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'recipes.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'recipes.wsgi.application'
 
 
 # Database
--- recipes/urls.py
+++ recipes/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import recipes_website.views
+import website.views
 
 # Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^$', 'recipes.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', recipes_website.views.index, name='index'),
-    url(r'^db', recipes_website.views.db, name='db'),
+    url(r'^$', website.views.index, name='index'),
+    url(r'^db', website.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
