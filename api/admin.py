28b0a29b667a6f49c471230cc9a3796146bd5d64
r3econ <rafal.sroka.it@gmail.com>
2017-12-02 20:00:13 +0100

    Add admin interfaces for ingredient, category and recipe

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,38 @@
+"""
+Admin interface
+"""
 from django.contrib import admin
 from api import models
 
-admin.site.register(models.Ingredient)
-admin.site.register(models.Category)
-admin.site.register(models.Recipe)
+class IngredientAdmin(admin.ModelAdmin):
+    """
+    Ingredient admin interface
+    """
+    fields = ['id', 'name']
+    readonly_fields = ['id']
+    list_display = ['name', 'id']
+    search_fields = ['name']
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category admin interface
+    """
+    fields = ['id', 'name', 'description']
+    readonly_fields = ['id']
+    list_display = ['name', 'id']
+    search_fields = ['name', 'description']
+
+class RecipeAdmin(admin.ModelAdmin):
+    """
+    Recipe admin interface
+    """
+    fields = ['id', 'title', 'description', 'author', 'cooking_time',
+              'preparation_time', 'serving_count']
+    readonly_fields = ['id']
+    list_display = ['title', 'author', 'id']
+    search_fields = ['title', 'description']
+
+# Register admin interfaces
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Ingredient, IngredientAdmin)
+admin.site.register(models.Recipe, RecipeAdmin)
