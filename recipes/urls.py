7fe0d5d90929d8de95a28b9daf7dd315453e7b26
r3econ <rafal.sroka.it@gmail.com>
2017-12-10 20:18:01 +0100

    Update endpoints

--- recipes/urls.py
+++ recipes/urls.py
@@ -15,15 +15,16 @@ urlpatterns = [
     # Website urls
     url(r'^$', website.views.index, name='index'),
     url(r'^recipes/(?P<recipe_id>[0-9]+)/$', website.views.recipe, name='recipe'),
-    # Restful API
-    # Following urls define the api
+    # Swagger schema
     url(r'^api/$', get_swagger_view(title='Recipes API')),
+    # Categories
     url(r'^api/categories/$', api.views.CategoryListView.as_view(), name='category-list'),
     url(r'^api/categories/(?P<category_id>\d+)/recipes/$', api.views.CategoryRecipeListView.as_view(), name='category-recipes-list'),
+    # Recipes
     url(r'^api/recipes/$', api.views.RecipeListView.as_view(), name='recipe-list'),
     url(r'^api/recipes/(?P<pk>\d+)/$', api.views.RecipeDetailView.as_view(), name='recipe-detail'),
     url(r'^api/recipes/(?P<recipe_id>\d+)/bookmark/$', api.views.RecipeBookmarkView.as_view(), name='recipe-bookmark'),
     url(r'^api/recipes/(?P<recipe_id>\d+)/bookmark/undo$', api.views.RecipeUnbookmarkView.as_view(), name='recipe-unbookmark'),
+    # Users
     url(r'^api/users/$', api.views.UserListView.as_view(), name='user-list'),
-
 ]
